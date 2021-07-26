b6f77c62d4f348e9a69652dffcf14d99a7e46f75
Kenny Ortega <kennyaoc@gmail.com>
2021-07-26 02:41:33 -0400

    Update views and order timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,9 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    @timeline_posts ||=  Post.where(user_id: [Friendship.friends(current_user.id)])
+
+
   end
 
   def post_params
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -9,4 +9,17 @@ class Friendship < ApplicationRecord
   def self.requested(request_id)
     where(friend_id: request_id)
   end
+
+  def self.friends(request_id)
+    my_friends = []
+    friends = Friendship.where(user_id: request_id).or(Friendship.where(friend_id: request_id)).where(status: 1)
+    friends.each do |users|
+      if users.user_id != request_id
+        my_friends.push(users.user_id)
+      else
+        my_friends.push(users.friend_id)
+      end
+    end
+    my_friends
+  end
 end
